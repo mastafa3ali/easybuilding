406814624426787f8f24252b0d31b7b9563be79f
mustafa <mustafa>
2023-03-06 00:21:06 +0200

    rent

--- app/Http/Resources/CompanyResource.php
+++ app/Http/Resources/CompanyResource.php
@@ -17,7 +17,7 @@ class CompanyResource extends JsonResource
             $data['price']            = $this->price;
         }
         $data['description']      = $this->description;
-        $data['rent_type']        = $this->rent_type ?? null;
+        $data['rent_type']        = $this->rent_type ? __('products.rent_types.'.$this->rent_type): null;
         $data['saved']            = User::saved($this->id);
         $data['image']            = $this->photo;
         return $data;
