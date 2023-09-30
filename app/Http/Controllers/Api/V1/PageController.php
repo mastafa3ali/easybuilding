b39cc4c10e821eb4679f44ffc1aed5c3670a5d14
Mustafa <mastafa3alie@gmail.com>
2023-09-30 11:51:55 +0200

    page

--- app/Http/Controllers/Api/V1/PageController.php
+++ app/Http/Controllers/Api/V1/PageController.php
@@ -53,9 +53,9 @@ class PageController extends Controller
     public function getSales($id)
     {
 
-        if(request()->sort_type==0) {
+        if(request()->sort_type == 0) {
             $sort = 'ASC';
-        } elseif(request()->sort_type==1) {
+        } elseif(request()->sort_type == 1) {
             $sort = 'DESC';
         }
 
@@ -73,7 +73,7 @@ class PageController extends Controller
                'users.phone',
                'users.description',
                'users.image'
-           ])->orderBy('id',$sort)
+           ])->orderBy('id', $sort)
         ->get();
 
 
@@ -114,21 +114,21 @@ class PageController extends Controller
         return apiResponse(true, $data, null, null, 200);
     }
 
-    public function getCompanies($id, $sort_type='ASC')
+    public function getCompanies($id, $sort_type = 'ASC')
     {
-        if(request()->sort_type==0) {
+        if(request()->sort_type == 0) {
             $sort = 'ASC';
-        } elseif(request()->sort_type==1) {
+        } elseif(request()->sort_type == 1) {
             $sort = 'DESC';
         }
         //بترجع الشركات اللى ضايفه عروض على المنتج للايجار
-        if(request()->sort_type==2) {
+        if(request()->sort_type == 2) {
             $data = User::where('users.type', User::TYPE_COMPANY)
             ->leftJoin('company_products', 'company_products.company_id', 'users.id')
             ->where('company_products.available', 1)
             ->leftJoin('products', 'products.id', 'company_products.product_id')
-               ->where('products.type', Product::TYPE_RENT)
-               ->where('products.id', $id)
+            ->where('products.type', Product::TYPE_RENT)
+            ->where('products.id', $id)
             ->select([
                 'company_products.price as price',
                 'company_products.price_2',
@@ -149,6 +149,7 @@ class PageController extends Controller
 
             $data = User::where('users.type', User::TYPE_COMPANY)
             ->leftJoin('company_products', 'company_products.company_id', 'users.id')
+            ->where('company_products.available', 1)
             ->leftJoin('products', 'products.id', 'company_products.product_id')
                ->where('products.type', Product::TYPE_RENT)
                ->where('products.id', $id)
@@ -174,7 +175,7 @@ class PageController extends Controller
     }
     public function getSavedProduct()
     {
-        $products= Product::leftJoin('saveds', 'model_id', 'products.id')->where('saveds.user_id', auth()->id())->where('saveds.model_type', Saved::TYPE_PRODUCT)->select([
+        $products = Product::leftJoin('saveds', 'model_id', 'products.id')->where('saveds.user_id', auth()->id())->where('saveds.model_type', Saved::TYPE_PRODUCT)->select([
             'saveds.model_type as model_type',
             'products.*',
         ])->get();
@@ -183,7 +184,7 @@ class PageController extends Controller
     }
     public function getSavedCompany()
     {
-        $companies= User::leftJoin('saveds', 'model_id', 'users.id')->where('saveds.user_id', auth()->id())->where('saveds.model_type', Saved::TYPE_COMPANY)->select([
+        $companies = User::leftJoin('saveds', 'model_id', 'users.id')->where('saveds.user_id', auth()->id())->where('saveds.model_type', Saved::TYPE_COMPANY)->select([
          'saveds.model_type as model_type',
          'users.*',
         ])->get();
@@ -193,17 +194,17 @@ class PageController extends Controller
     }
     public function getCompanyProduct($id, $category_id)
     {
-        if(request()->sort_type==0) {
+        if(request()->sort_type == 0) {
             $sort = 'ASC';
-        } elseif(request()->sort_type==1) {
+        } elseif(request()->sort_type == 1) {
             $sort = 'DESC';
         }
 
-        if(request()->sort_type==2) {
-            $products = Product::where('available',1)
+        if(request()->sort_type == 2) {
+            $products = Product::where('available', 1)
         ->with('subcategory')->where('category_id', $category_id)->where('company_id', $id)->orderBy('rate', 'DESC')->get();
         } else {
-            $products = Product::where('available',1)
+            $products = Product::where('available', 1)
         ->with('subcategory')->where('category_id', $category_id)->where('company_id', $id)->orderBy('price', $sort)->get();
         }
 
@@ -213,9 +214,9 @@ class PageController extends Controller
     public function notifications()
     {
         $data = [];
-        $dayes = ApiNotification::where('user_id', auth()->id())->groupBy('day')->orderBy('id','DESC')->pluck('day')->toArray();
+        $dayes = ApiNotification::where('user_id', auth()->id())->groupBy('day')->orderBy('id', 'DESC')->pluck('day')->toArray();
         foreach($dayes as $day) {
-            $list = ApiNotification::where('user_id', auth()->id())->where('day', $day)->orderBy('id','DESC')->get();
+            $list = ApiNotification::where('user_id', auth()->id())->where('day', $day)->orderBy('id', 'DESC')->get();
             $data[]['day'] = $list;
         }
         return apiResponse(true, $data, null, null, 200);
@@ -228,47 +229,47 @@ class PageController extends Controller
     public function makeSaved(Request $request)
     {
         $data = [
-            'model_id'=>$request->model_id,
-            'model_type'=>$request->model_type,
-            'user_id'=>auth()->id()
+            'model_id' => $request->model_id,
+            'model_type' => $request->model_type,
+            'user_id' => auth()->id()
         ];
         $item_name = '';
-        if($request->model_type==Saved::TYPE_COMPANY) {
+        if($request->model_type == Saved::TYPE_COMPANY) {
             $item = User::find($request->model_id);
             $item_name = $item->name;
         }
-        if($request->model_type==Saved::TYPE_PRODUCT) {
+        if($request->model_type == Saved::TYPE_PRODUCT) {
             $item = Product::find($request->model_id);
             $item_name = $item->name;
         }
-        $is_saved=Saved::where('model_id', $request->model_id)
+        $is_saved = Saved::where('model_id', $request->model_id)
             ->where('model_type', $request->model_type)
             ->where('user_id', auth()->id())->first();
         if($is_saved) {
             $is_saved->delete();
-            $message = __('api.removed_from_saved_success', ['item'=>$item_name]);
+            $message = __('api.removed_from_saved_success', ['item' => $item_name]);
             $fcmTokens = [auth()->user()->fcm_token];
             $notifications = [
-                'user_id'=>auth()->id(),
-                'text'=>$message,
-                'day'=>date('Y-m-d'),
-                'time'=>date('H:i'),
+                'user_id' => auth()->id(),
+                'text' => $message,
+                'day' => date('Y-m-d'),
+                'time' => date('H:i'),
             ];
             ApiNotification::create($notifications);
             Notification::send(null, new SendPushNotification($message, $fcmTokens));
             return apiResponse(true, null, $message, null, 200);
         }
-        $saved=Saved::create($data);
+        $saved = Saved::create($data);
         $message = '';
         if($saved) {
 
-            $message = __('api.add_to_saved', ['item'=>$item_name]);
+            $message = __('api.add_to_saved', ['item' => $item_name]);
             $fcmTokens = [auth()->user()->fcm_token];
             $notifications = [
-                'user_id'=>auth()->id(),
-                'text'=>$message,
-                'day'=>date('Y-m-d'),
-                'time'=>date('H:i'),
+                'user_id' => auth()->id(),
+                'text' => $message,
+                'day' => date('Y-m-d'),
+                'time' => date('H:i'),
             ];
             ApiNotification::create($notifications);
             Notification::send(null, new SendPushNotification($message, $fcmTokens));
@@ -279,24 +280,24 @@ class PageController extends Controller
 
     public function search(Request $request)
     {
-        $data=['companies'=>[],'products'=>[]];
+        $data = ['companies' => [],'products' => []];
         if($request->filled('name')) {
-            $companies=User::where('name', 'like', '%'.$request->name.'%')->get();
-            $products=Product::where('available',1)
-        ->where('name', 'like', '%'.$request->name.'%')->get();
+            $companies = User::where('name', 'like', '%' . $request->name . '%')->get();
+            $products = Product::where('available', 1)
+        ->where('name', 'like', '%' . $request->name . '%')->get();
             $result = [];
             $allcompanies = [];
             $doublicate = [];
-            foreach($companies as $company){
+            foreach($companies as $company) {
                 $allcompanies[] = new CompanyResource($company);
             }
-            foreach($products as $product){
-                if($product->type==Product::TYPE_RENT){
+            foreach($products as $product) {
+                if($product->type == Product::TYPE_RENT) {
                     $result[] = new ProductResource($product);
-                }else{
-                    $company=User::where('id', $product->company_id)->first();
-                    if($company){
-                        if(!in_array($company->id,$doublicate)){
+                } else {
+                    $company = User::where('id', $product->company_id)->first();
+                    if($company) {
+                        if(!in_array($company->id, $doublicate)) {
                             $doublicate[] = $company->id;
                             $allcompanies[] = new CompanyResource($company);
                         }
@@ -366,19 +367,19 @@ class PageController extends Controller
     }
     public function contactUs(ContactRequest $request)
     {
-        $data=Contact::create([
-            'name'=>$request->name,
-            'reason'=>$request->reason,
-            'email'=>$request->email,
-            'phone'=>$request->phone,
-            'problem'=>$request->message,
+        $data = Contact::create([
+            'name' => $request->name,
+            'reason' => $request->reason,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'problem' => $request->message,
         ]);
         return apiResponse(true, $data, "", null, 200);
     }
-    public function getCompanyPayment($id,$type=1)
+    public function getCompanyPayment($id, $type = 1)
     {
         //sale=1, rent=2
-        $data=Payment::where('company_id', $id)->where('type',$type)->get();
+        $data = Payment::where('company_id', $id)->where('type', $type)->get();
         return apiResponse(true, $data, "", null, 200);
     }
 
