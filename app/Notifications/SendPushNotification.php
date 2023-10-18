e9af4dc8f461c411da381d3ec2258e4f54f622bb
Mustafa <mastafa3alie@gmail.com>
2023-10-18 19:38:20 +0200

    check

--- app/Notifications/SendPushNotification.php
+++ app/Notifications/SendPushNotification.php
@@ -2,7 +2,9 @@
 
 namespace App\Notifications;
 
+use App\Models\User;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Notification;
 use Kutia\Larafirebase\Messages\FirebaseMessage;
 
@@ -19,9 +21,9 @@ class SendPushNotification extends Notification
      *
      * @return void
      */
-    public function __construct($message,$fcmTokens)
+    public function __construct($message, $fcmTokens)
     {
-        $this->title = __('api.notification_title');
+        $this->title = "Aldon";
         $this->message = $message;
         $this->fcmTokens = $fcmTokens;
     }
@@ -39,53 +41,48 @@ class SendPushNotification extends Notification
 
     public function toFirebase($notifiable)
     {
-        return (new FirebaseMessage)
-            ->withTitle($this->title)
-            ->withBody($this->message)
-            ->withPriority('high')->asMessage($this->fcmTokens);
+
+        $url = 'https://fcm.googleapis.com/fcm/send';
+
+        $serverKey = config('larafirebase.authentication_key');
+
+        $data = [
+            "registration_ids" => $this->fcmTokens,
+            "notification" => [
+                "title" => $this->title,
+                "body" => $this->message,
+            ]
+        ];
+        $encodedData = json_encode($data);
+
+        $headers = [
+            'Authorization:key=' . $serverKey,
+            'Content-Type: application/json',
+        ];
+
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+        // Disabling SSL Certificate support temporarly
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $encodedData);
+        // Execute post
+        $result = curl_exec($ch);
+        if ($result === false) {
+            die('Curl failed: ' . curl_error($ch));
+        }
+        // Close connection
+        curl_close($ch);
+
+
+        // return (new FirebaseMessage)
+        //     ->withTitle($this->title)
+        //     ->withBody($this->message)
+        //     ->withPriority('high')->asMessage($this->fcmTokens);
     }
-
-
-//  $url = 'https://fcm.googleapis.com/fcm/send';
-
-//         $serverKey = 'AAAAfiKRrcA:APA91bGlCKkWNlXc7S72tP5E9nVyJV95Lg4CUOVcZvucXXw3173ywojLl6U3t3awlphy2gNGhBCVoWTi28MYJM379Zzt0V0m6VgkUAAiCSgq4sPsTqWOyqryQjTNS5wvHpw54WK_b67J';
-
-//         $data = [
-//             "registration_ids" => $fcmTokens,
-//             "notification" => [
-//                 "title" => $title,
-//                 "body" => $message,
-//                 "date" => date('H:i'),
-//             ]
-//         ];
-//         $encodedData = json_encode($data);
-
-//         $headers = [
-//             'Authorization:key=' . $serverKey,
-//             'Content-Type: application/json',
-//         ];
-
-//         $ch = curl_init();
-
-//         curl_setopt($ch, CURLOPT_URL, $url);
-//         curl_setopt($ch, CURLOPT_POST, true);
-//         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-//         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-//         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-//         curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
-//         // Disabling SSL Certificate support temporarly
-//         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-//         curl_setopt($ch, CURLOPT_POSTFIELDS, $encodedData);
-//         // Execute post
-//         $result = curl_exec($ch);
-//         if ($result === FALSE) {
-//             die('Curl failed: ' . curl_error($ch));
-//         }
-//         // Close connection
-//         curl_close($ch);
-//         // FCM response
-//         // dd($result);
-
-
-
 }
